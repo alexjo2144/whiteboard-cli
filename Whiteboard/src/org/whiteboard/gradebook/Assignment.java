ac4c3472d5877ab89e446a99d178615ed1b0d5c5
alexjo2144 <jo.al@husky.neu.edu>
2014-04-07 15:25:58 -0400

    added setters and getters for Assignments

--- Whiteboard/src/org/whiteboard/gradebook/Assignment.java
+++ Whiteboard/src/org/whiteboard/gradebook/Assignment.java
@@ -11,7 +11,9 @@ import java.util.Map;
  * @author Daniel Wolf <wolf@ccs.neu.edu>
  * @version Mar 30, 2014 */
 public abstract class Assignment {
+    /** String representing the title for this assignment */
     private String name;
+    /** String used as a short description of this assignment */
     private String description;
     private String kind;
     private Integer totalPointsPossible;
@@ -22,4 +24,36 @@ public abstract class Assignment {
     public abstract Double calculateAverage(User actor);
     public abstract Double calculateMedian(User actor);
     public abstract Double calculateStandardDeviation(User actor);
+    
+    protected String getName() {
+        return this.name;
+    }
+    
+    protected void setName(String n) {
+        this.name = n;
+    }
+
+    protected String getDescription() {
+        return this.description;
+    }
+    
+    protected void setDescription(String d) {
+        this.description = d;
+    }
+    
+    protected String getKind() {
+        return this.kind;
+    }
+    
+    protected void setKind(String k) {
+        this.kind = k;
+    }
+    
+    protected int getTotalPointsPossible() {
+        return this.totalPointsPossible;
+    }
+    
+    protected void setTotalPointsPossible(int p) {
+        this.totalPointsPossible = p;
+    }
 }
