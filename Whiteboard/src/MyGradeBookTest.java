8b7d99c8a7cfdedcc31b55565cd52b65a42a3264
Jacob Ginsparg <jacobginsparg@mail.com>
2014-04-11 14:57:59 -0400

    here is my try, here is my catch

--- Whiteboard/src/MyGradeBookTest.java
+++ Whiteboard/src/MyGradeBookTest.java
@@ -1,5 +1,6 @@
+import static org.junit.Assert.*;
+import java.io.FileNotFoundException;
 import java.util.NoSuchElementException;
-
 import org.whiteboard.gradebook.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,11 +24,47 @@ public class MyGradeBookTest {
     private Assignment myAss3;
     private Assignment myAss4;
 
+    /**
+     * An Empty kool GradeBook
+     */
     private MyGradeBook kGB0;
+    /**
+     * A kool GradeBook from initial.txt
+     */
     private MyGradeBook kGB1;
+    /**
+     * A kool GradeBook with assignments from addAssignments.txt
+     */
     private MyGradeBook kGB2;
+    /**
+     * A kool GradeBook with students from addStudents.txt
+     */
     private MyGradeBook kGB3;
+    /**
+     * A kool GradeBook with both
+     */
     private MyGradeBook kGB4;
 
+    /**
+     * Sets initials
+     */
+    @Before
+    public void initials() {
+        
+        // Gradebooks
+        this.kGB0 = MyGradeBook.initialize();
+        try {
+            this.kGB1 = MyGradeBook.initializeWithFile("initial.txt");
+        }
+        catch (FileNotFoundException e) {
+            // The file was not found
+            assertTrue(false);
+        }
+        
+        // Assignments
+        
+        
+        // Grades
+    }
 
 }
