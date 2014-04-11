db7c551fb3cbbbcb48de1c296b801e8311a8093a
Jacob Ginsparg <jacobginsparg@mail.com>
2014-04-11 14:22:51 -0400

    Put in basic shell

---
+++ Whiteboard/src/MyGradeBookTest.java
@@ -0,0 +1,33 @@
+import java.util.NoSuchElementException;
+
+import org.whiteboard.gradebook.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.*;
+import org.junit.runners.JUnit4;
+
+/**
+ * Testing suite for Black Box tests
+ * 
+ * @author jacobginsparg <jgins@ccs.neu.edu>
+ * @version 4/11/2014
+ */
+@RunWith(JUnit4.class)
+public class MyGradeBookTest {
+
+    //////////////////////////Private Fields /////////////////////////////////
+
+    private Assignment myAss0;
+    private Assignment myAss1;
+    private Assignment myAss2;
+    private Assignment myAss3;
+    private Assignment myAss4;
+
+    private MyGradeBook kGB0;
+    private MyGradeBook kGB1;
+    private MyGradeBook kGB2;
+    private MyGradeBook kGB3;
+    private MyGradeBook kGB4;
+
+
+}
